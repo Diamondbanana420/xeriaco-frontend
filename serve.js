5077208cecdc7bcd6a5361ebe22485457c6656ee
Diamondbanana420 <benjamesbradshaw22@gmail.com>
2026-02-08 13:00:53 +1000

    V9: Fix serve.js to use CommonJS require

--- serve.js
+++ serve.js
@@ -1,22 +1,17 @@
-import express from 'express';
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(join(__dirname, 'dist'), {
+app.use(express.static(path.join(__dirname, 'dist'), {
   maxAge: '1d',
   etag: true,
 }));
 
 app.get('*', (req, res) => {
-  res.sendFile(join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`XeriaCo V9 serving on port ${PORT}`);
+  console.log('XeriaCo V9 serving on port ' + PORT);
 });
