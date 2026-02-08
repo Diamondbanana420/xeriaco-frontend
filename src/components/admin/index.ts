4555d829149d64da731721522285cd73e21b335e
Diamondbanana420 <benjamesbradshaw22@gmail.com>
2026-02-08 10:02:13 +1000

    V9 cleanup: remove src/components/admin/index.ts

--- src/components/admin/index.ts
+++
@@ -1,3 +0,0 @@
-// Export all admin components for easy imports
-export { AdminLayout } from './AdminLayout';
-export { MetricCard, RevenueChart, PipelineStatus, RecentOrders, TopProducts } from './AdminComponents';
